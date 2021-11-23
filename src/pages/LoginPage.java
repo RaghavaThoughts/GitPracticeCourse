70b3aa9ab456dbbfecdef8209322318d85154eaa
RaghavaThoughts <pashamraghava414@gmail.com>
2021-11-23 22:39:50 +0530

    new file added/updated login/deleted hometest

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -4,6 +4,9 @@ public class LoginPage {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello LP");
+		
+		String url="admin";
+		String pwd="admin";
 	}
 
 }
---
+++ src/pages/SearchPage.java
@@ -0,0 +1,12 @@
+package pages;
+
+public class SearchPage {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Search Page");
+
+	}
+
+}
